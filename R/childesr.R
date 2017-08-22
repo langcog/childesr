715c308157acb8253d699e8ecf87188f477061ff
dyurovsky <dyurovsky@gmail.com>
2017-08-22 15:58:07 -0700

    changed order of dbplyr filters to speed up db access

--- R/childesr.R
+++ R/childesr.R
@@ -210,7 +210,7 @@ get_participants <- function(collection = NULL, corpus = NULL, child = NULL,
 #' @inheritParams get_participants
 get_content <- function(content_type, collection = NULL, corpus = NULL,
                         role = NULL, age = NULL, sex = NULL, child = NULL,
-                        connection) {
+                        token = NULL, connection) {
 
   transcripts <- get_transcripts(collection, corpus, child, connection)
   corpora <- transcripts %>%
@@ -229,6 +229,12 @@ get_content <- function(content_type, collection = NULL, corpus = NULL,
 
   content <- dplyr::tbl(connection, content_type)
 
+  if (content_type == "token" & !is.null(token)) {
+    token_filter <- sprintf("gloss %%like%% '%s'", token) %>%
+      paste(collapse = " | ")
+    content %<>% dplyr::filter_(token_filter)
+  }
+
   if (!num_corpora) {
     corpus_filter <- -1
     child_id <- -1
@@ -240,8 +246,8 @@ get_content <- function(content_type, collection = NULL, corpus = NULL,
     content %<>% dplyr::filter(corpus_id %in% corpus_filter)
   }
 
-  if (!is.null(role)) {
-    content %<>% dplyr::filter(speaker_role %in% role)
+  if (!is.null(child)) {
+    content %<>% dplyr::filter(target_child_id %in% child_id)
   }
 
   if (!is.null(age)) {
@@ -261,8 +267,9 @@ get_content <- function(content_type, collection = NULL, corpus = NULL,
     content %<>% dplyr::filter(sex %in% sex_filter)
   }
 
-  if (!is.null(child)) {
-    content %<>% dplyr::filter(target_child_id %in% child_id)
+
+  if (!is.null(role)) {
+    content %<>% dplyr::filter(speaker_role %in% role)
   }
 
   return(content)
@@ -296,13 +303,8 @@ get_tokens <- function(collection = NULL, corpus = NULL, child = NULL,
                         age = age,
                         sex = sex,
                         child = child,
+                        token = token,
                         connection = con)
-
-  if (!is.null(token)) {
-    token_filter <- sprintf("gloss %%like%% '%s'", token) %>%
-      paste(collapse = " | ")
-    tokens %<>% dplyr::filter_(token_filter)
-  }
 
   if (is.null(connection)) {
     tokens %<>% dplyr::collect()
